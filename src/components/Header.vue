1c65d49b746e7d0883585435325c52f1027b2b9a
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-03-07 13:22:02 +0300

    07.03d-c1

---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="row header_row">
+        <div class="col-3">
+          <div class="olympic_logo__box">
+            <div class="row">
+              <div class="col-3">
+                <a href="#" class="header__brand">
+                  <img src="./../../public/images/navbar/logo.png" alt="" />
+                </a>
+              </div>
+              <div class="col-9">
+                <h3>O‘zbekiston Milliy Olimpiya Qo‘mitasi</h3>
+                <ul>
+                  <li>CITIUS</li>
+                  <li>CALTIUS</li>
+                  <li>FORTIUS</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-9">
+          <div class="olympic_links__box">
+            <div class="olympic_links_above__box">
+              <ul>
+                <li>Virtual Qabulxona</li>
+                <li>Elonlar</li>
+                <li>Media Galeriya</li>
+                <li>Izlash</li>
+              </ul>
+              <ul>
+                <li>Русский</li>
+                <li>English</li>
+                <li>Узбекча</li>
+                <li>O'zbekcha</li>
+              </ul>
+            </div>
+            <hr />
+            <div class="olympic_links_bottom__box">
+              <ul>
+                <li>O'zbekiston MOQ</li>
+                <li>Yangiliklar</li>
+                <li>Federatsiyalar</li>
+                <li>O'yinlar</li>
+                <li>Jahon</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped></style>
