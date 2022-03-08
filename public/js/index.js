632bf7d743a638b966b1aec845110f73fabcf66d
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-03-08 17:19:51 +0300

    08.03d-c3

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,45 @@ $(function() {
         dots: false,
         infinite: true,
         fade:true,
-        speed: 400,
-        // autoplay:true,
+        speed: 300,
+        autoplay:true,
         slidesToShow: 1,
         adaptiveHeight: true
+      });
+  });
+
+  $(function() {
+    $('.banner_foter__box').slick({
+        dots: false,
+  infinite: true,
+  speed: 300,
+  autoplay:true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
       });
   });
--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -21,6 +21,13 @@
           src="./../../public/images/banner/b2.jpg"
           alt=""
         />
+        <div class="banner_item__text">
+          <div class="container">
+            <p><i class="lnr lnr-history"></i> 22 Fevral 2022</p>
+            <h3>Sportni rivojlantirish sommiti yakunlandi</h3>
+            <button class="btn btn-primary">Batafsil</button>
+          </div>
+        </div>
       </div>
       <div class="banner__item col-3">
         <img
@@ -28,6 +35,16 @@
           src="./../../public/images/banner/b3.jpg"
           alt=""
         />
+        <div class="banner_item__text">
+          <div class="container">
+            <p><i class="lnr lnr-history"></i> 13 Yanvar 2022</p>
+            <h3>
+              osh qilichbozlarimiz Osiyo chempionatini 15 ta medal bilan
+              yakunlashdi!
+            </h3>
+            <button class="btn btn-primary">Batafsil</button>
+          </div>
+        </div>
       </div>
       <div class="banner__item col-3">
         <img
@@ -35,14 +52,63 @@
           src="./../../public/images/banner/b4.jpg"
           alt=""
         />
+        <div class="banner_item__text">
+          <div class="container">
+            <p><i class="lnr lnr-history"></i> 3 Mart 2022</p>
+            <h3>Futbol bo'yicha OPLsi boshlandi!</h3>
+            <button class="btn btn-primary">Batafsil</button>
+          </div>
+        </div>
       </div>
     </div>
     <div class="banner__footer">
       <div class="container">
-        <div class="banner_footer__item col-3">
-          <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
-          <hr />
-          <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+        <div class="banner_foter__box row">
+          <div class="banner_footer__item col-3">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <hr />
+            <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+          </div>
+          <div class="banner_footer__item col-3">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <hr />
+            <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+          </div>
+          <div class="banner_footer__item col-3">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <hr />
+            <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+          </div>
+          <div class="banner_footer__item col-3">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <hr />
+            <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+          </div>
+          <div class="banner_footer__item col-3">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <hr />
+            <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+          </div>
+          <div class="banner_footer__item col-3">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <hr />
+            <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+          </div>
+          <div class="banner_footer__item col-3">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <hr />
+            <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+          </div>
+          <div class="banner_footer__item col-3">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <hr />
+            <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+          </div>
+          <div class="banner_footer__item col-3">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <hr />
+            <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+          </div>
         </div>
       </div>
     </div>
