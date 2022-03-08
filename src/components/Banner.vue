af89d53fc61358b61d3cae138754e4d53e26243d
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-03-08 15:54:50 +0300

    08.03d-c1

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,17 +1,47 @@
 <template>
   <section class="banner">
-    <div class="banner__wrapper row">
-      <div class="banner__item col-3">
-        <img src="./../../public/images/banner/b1.jpg" alt="" />
+    <div class="container">
+      <div class="banner__wrapper row">
+        <div class="banner__item col-3">
+          <img
+            class="banner_item__img img-fluid"
+            src="./../../public/images/banner/b1.jpg"
+            alt=""
+          />
+          <div class="banner_item__text">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+            <button class="btn btn-primary">Batafsil</button>
+          </div>
+        </div>
+        <div class="banner__item col-3">
+          <img
+            class="banner_item__img img-fluid"
+            src="./../../public/images/banner/b2.jpg"
+            alt=""
+          />
+        </div>
+        <div class="banner__item col-3">
+          <img
+            class="banner_item__img img-fluid"
+            src="./../../public/images/banner/b3.jpg"
+            alt=""
+          />
+        </div>
+        <div class="banner__item col-3">
+          <img
+            class="banner_item__img img-fluid"
+            src="./../../public/images/banner/b4.jpg"
+            alt=""
+          />
+        </div>
       </div>
-      <div class="banner__item col-3">
-        <img src="./../../public/images/banner/b2.jpg" alt="" />
-      </div>
-      <div class="banner__item col-3">
-        <img src="./../../public/images/banner/b3.jpg" alt="" />
-      </div>
-      <div class="banner__item col-3">
-        <img src="./../../public/images/banner/b4.jpg" alt="" />
+      <div class="banner__footer">
+        <div class="banner_footer__item col-3">
+          <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+          <hr />
+          <h3>MOQ ayollar jamoasiga ehtirom ko‘rsatildi</h3>
+        </div>
       </div>
     </div>
   </section>
