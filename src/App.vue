37fd13ea44ef3b0d03272090e4f218a754b54a37
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-03-08 22:29:23 +0300

    08.03d-c8

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <Header />
   <Banner />
   <News />
+  <Interview />
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Banner from "./components/Banner.vue";
 import News from "./components/News.vue";
+import Interview from "./components/Interview.vue";
 
 export default {
   name: "App",
   components: {
+    Interview,
     News,
     Banner,
     Header,
---
+++ src/components/Interview.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="interview__section">
+    <div class="container">
+      <div class="news__head">
+        <h3>Intervyu</h3>
+        <button class="btn btn-outline-primary">Barcha intervyular</button>
+      </div>
+      <div class="row">
+        <div class="col-7">
+          <a class="interview_col_7_textpart btn">
+            <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <h4>
+              Olimjon Hikmatov: "Asosiy vazifa - Osiyo o‘yinlariga kuchli tarkib
+              bilan borish!"
+            </h4>
+          </a>
+        </div>
+        <div class="col-5">
+          <div class="row">
+            <a class="interview_row__one btn">
+              <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+              <h4>
+                Shuhrat Salayev: "Keyingi yilgi "Turkish Open" musobaqasida
+                oltin medal olishni maqsad qildim"
+              </h4>
+            </a>
+            <a class="interview_row__two btn">
+              <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+              <h4>Komiljon To'xtayev: "Oson emas! Juda qiyin bo'ldi"</h4>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Interview",
+};
+</script>
+
+<style scoped></style>
