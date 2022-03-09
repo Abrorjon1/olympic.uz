0650bec4ae7e5dcd0e72c2e5bd21c14174728c36
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-03-09 13:08:06 +0300

    09.03d-c6

--- src/components/SportTypes.vue
+++ src/components/SportTypes.vue
@@ -19,6 +19,118 @@
             </div>
           </a>
         </div>
+        <div class="col-md-3">
+          <a class="btn">
+            <div class="row">
+              <div class="col-md-4 sport_text__box">
+                <h5>Sport kurashi</h5>
+                <p><i class="lnr lnr-sun"></i> Yozgi</p>
+              </div>
+              <div class="col-md-4">
+                <img
+                  src="https://olympic.uz/uploads/sports_types/d58b588e7d04dc609e90e0d9334bb4c1.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a class="btn">
+            <div class="row">
+              <div class="col-md-4 sport_text__box">
+                <h5>Og'ir atletika</h5>
+                <p><i class="lnr lnr-sun"></i> Yozgi</p>
+              </div>
+              <div class="col-md-4">
+                <img
+                  src="https://olympic.uz/uploads/sports_types/55f9ace0954e0ea5aa60fdaa4e165ab8.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a class="btn">
+            <div class="row">
+              <div class="col-md-4 sport_text__box">
+                <h5>Voleybol</h5>
+                <p><i class="lnr lnr-sun"></i> Yozgi</p>
+              </div>
+              <div class="col-md-4">
+                <img
+                  src="	https://olympic.uz/uploads/sports_types/468e31eff06148c7f57201fbe6df2bf1.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a class="btn">
+            <div class="row">
+              <div class="col-md-4 sport_text__box">
+                <h5>Triatlon</h5>
+                <p><i class="lnr lnr-sun"></i> Yozgi</p>
+              </div>
+              <div class="col-md-4">
+                <img
+                  src="	https://olympic.uz/uploads/sports_types/1e466f8a658af9d1ff78b9015c159db2.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a class="btn">
+            <div class="row">
+              <div class="col-md-4 sport_text__box">
+                <h5>Tennis</h5>
+                <p><i class="lnr lnr-sun"></i> Yozgi</p>
+              </div>
+              <div class="col-md-4">
+                <img
+                  src="https://olympic.uz/uploads/sports_types/a9257848e30bb59126050ba831bc8d28.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a class="btn">
+            <div class="row">
+              <div class="col-md-4 sport_text__box">
+                <h5>Taekvondo</h5>
+                <p><i class="lnr lnr-sun"></i> Yozgi</p>
+              </div>
+              <div class="col-md-4">
+                <img
+                  src="https://olympic.uz/uploads/sports_types/ab3dacc7b5c28df9d8bad6af7cf9d045.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-3">
+          <a class="btn">
+            <div class="row">
+              <div class="col-md-4 sport_text__box">
+                <h5>Suv Sporti</h5>
+                <p><i class="lnr lnr-sun"></i> Yozgi</p>
+              </div>
+              <div class="col-md-4">
+                <img
+                  src="https://olympic.uz/uploads/sports_types/94dcdfb1ccaed5ddb59e6d7968eb41fe.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   </section>
