efcca950921b2165905852d79a10b2153eaa42bd
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-03-09 11:45:07 +0300

    09.03d-c3

--- public/js/index.js
+++ public/js/index.js
@@ -52,10 +52,10 @@ $(function() {
 //     });
 
 
-function openCity(evt, cityName) {
+function openCity(evt, cityName = 'Paris') {
   // Declare all variables
   var i, tabcontent, tablinks;
-
+ console.log(evt)
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
--- src/components/MediaFile.vue
+++ src/components/MediaFile.vue
@@ -7,7 +7,7 @@
           <button class="tablinks" onclick="openCity(event, 'London')">
             FotoGaleriya
           </button>
-          <button class="tablinks" onclick="openCity(event, 'Paris')">
+          <button class="tablinks active" onclick="openCity(event, 'Paris')">
             VideoGaleriya
           </button>
         </div>
@@ -43,29 +43,29 @@
         </div>
       </div>
 
-      <div id="Paris" class="tabcontent">
+      <div id="Paris" class="tabcontent d-block">
         <div class="row interview_m__row">
           <div class="col-7 ml-0">
-            <a class="interview_col_7_textpart btn">
-              <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
+            <a class="interview_col_7_textpart media__video_content__one btn">
+              <p><i class="lnr lnr-history"></i> 15 Yanvar 2022</p>
               <h4>
-                Mirjalol Salimov: "Asosiy vazifa - Osiyo o‘yinlariga kuchli
-                tarkib bilan borish!"
+                MOQ rahbarining muvaffaqiyatli urinishi Oʻzbekiston
+                chempionatini ochib berdi"
               </h4>
             </a>
           </div>
           <div class="col-5 mr-0">
             <div class="row">
-              <a class="interview_row__one btn">
-                <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
-                <h4>
-                  Shuhrat Salayev: "Keyingi yilgi "Turkish Open" musobaqasida
-                  oltin medal olishni maqsad qildim"
-                </h4>
+              <a class="interview_row__one media__video_content__two btn">
+                <p><i class="lnr lnr-history"></i> 31 Mart 2022</p>
+                <h4>"Save Aral" endi Toshkentda!</h4>
               </a>
-              <a class="interview_row__two btn">
-                <p><i class="lnr lnr-history"></i> 2 Mart 2022</p>
-                <h4>Komiljon To'xtayev: "Oson emas! Juda qiyin bo'ldi"</h4>
+              <a class="interview_row__two media__video_content__three btn">
+                <p><i class="lnr lnr-history"></i> 5 Iyun 2022</p>
+                <h4>
+                  Yugurish bo'yicha O'zbekistonga kirib kelayotgan Jahon sporti
+                  !
+                </h4>
               </a>
             </div>
           </div>
