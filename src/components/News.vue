7d19bf1e54f04076ea836031d8cab694b9346817
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-03-11 16:36:13 +0300

    11.03d-c2

--- src/components/News.vue
+++ src/components/News.vue
@@ -66,7 +66,7 @@
                 <i class="fas fa-chevron-left"></i>
               </button>
               <div class="slider_counter">
-                <span class="first__count">{{ this.id }} /</span>
+                <span class="first__count">1 /</span>
                 <span class="overall__count">3</span>
               </div>
               <button
@@ -136,53 +136,25 @@
           </div>
         </div>
       </div>
-      <swiper ref="mySwiper" :options="swiperOptions">
+      <!-- <swiper ref="mySwiper" :options="swiperOptions">
         <swiper-slide>Slide 1</swiper-slide>
         <swiper-slide>Slide 2</swiper-slide>
         <swiper-slide>Slide 3</swiper-slide>
         <swiper-slide>Slide 4</swiper-slide>
         <swiper-slide>Slide 5</swiper-slide>
         <div class="swiper-pagination" slot="pagination"></div>
-      </swiper>
+      </swiper> -->
     </div>
   </section>
 </template>
 
 <script>
-import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
-import "swiper/css/swiper.css";
+// import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
+// import "swiper/css/swiper.css";
 
 export default {
   name: "News",
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
-  directives: {
-    swiper: directive,
-  },
-  data() {
-    return {
-      id: 1,
-      swiperOptions: {
-        pagination: {
-          el: ".swiper-pagination",
-        },
-        // Some Swiper option/callback...
-      },
-    };
-  },
-  computed: {
-    swiper() {
-      return this.$refs.mySwiper.$swiper;
-    },
-  },
-  mounted() {
-    console.log("Current Swiper instance object", this.swiper);
-    this.swiper.slideTo(3, 1000, false);
-  },
-};
-Vue.use(window.VueAwesomeSwiper);
+  }
 </script>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
+import { createApp } from "vue";
 import App from "./App.vue";
-import Vue from "vue";
-// import App from "./App.vue";
-import router from "./router";
 import("./assets/index.css");
-Vue.config.productionTip = false;
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
+createApp(App).mount("#app");
